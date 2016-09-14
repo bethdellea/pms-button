490bd95309fd744b8ca5b580acf4e55201514b58
bethdellea <bethdellea@gmail.com>
2016-09-14 13:07:51 -0400

    Tweepy test is up and running
    Next step: ehhhh there are a lot of things to do, let's not order them

---
+++ tweepytst/tweepytest.py
@@ -0,0 +1,35 @@
+'''9/14/2016
+   python twitter bot testing!!!
+   '''
+
+import tweepy
+import random
+import os
+
+
+with open("secrets.txt") as f:
+    secrets = f.readlines()
+f.close()
+
+#set authorization variables
+CONSUMER_KEY = secrets[0].strip('\n')
+CONSUMER_SECRET = secrets[1].strip('\n')
+ACCESS_TOKEN = secrets[2].strip('\n')
+ACCESS_SECRET = secrets[3].strip('\n')
+
+#tweepy needs that info
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+#now we are connected to tweepy, time to make some tweets
+tweetlist = ["Woah! A computer made this tweet!",
+             "This is a randomly generated tweet!",
+             "We're learning about the twitter API!"]
+
+atlist = ["@weareicwc","@bethdellea", "@meataphor"]
+
+contentNum = random.randint(0, len(tweetlist))
+atNum = random.randint(0, len(atlist))
+tweet = atlist[atNum] + " " + tweetlist[contentNum]  #random text plus random directed user, for funsies
+api.update_status(tweet)
