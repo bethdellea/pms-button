07ed9c7631fa44f020a9e060dbd1c6e742becb4b
Beth Dellea <bethdellea@gmail.com>
2016-10-17 13:08:16 -0400

    about container sizing
    now linking by id will work much better

---
+++ htmlWorking/script/about.js
@@ -0,0 +1,21 @@
+//containers on the about page become a full page's height for optimal linking to by section
+function resolveFullHeight() {
+    $(".about-container").css("height", "auto");
+
+    var h_window = $(window).height(),
+        h_document = $(document).height(),
+        fullHeight_top = $(".about-container").position().top,
+        est_footerHeight = 112;
+
+   // var h_fullHeight = (-1 * (est_footerHeight + (fullHeight_top - h_document)));
+
+    //$(".about-container").height(h_fullHeight);
+        $(".about-container").height(h_window);
+
+}
+
+resolveFullHeight();
+
+$(window).resize(function () {
+    resolveFullHeight();
+});
