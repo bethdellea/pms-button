370b6243bfcd3d0550aa8c0525e3f994cd5e7583
K Sadwin <klizzinator@gmail.com>
2016-10-18 22:53:37 -0400

    added district table

--- webTest/app/models.py
+++ webTest/app/models.py
@@ -13,26 +13,41 @@ class State(db.Model):
         self.abbr = abbr
 
 
+class District(db.Model):
+    __tablename__ = 'district'
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer)
+    state_id = db.Column(db.Integer, db.ForeignKey('state.id'))
+    state = db.relationship('state', foreign_keys=[state_id])
+
+    def __init__(self, state, number):
+        self.state = state
+        self.number = number
+
+
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32))
     pass_hash = db.Column(db.String)
-    zip = db.Column(db.String(5))
+    zip_code = db.Column(db.String(5))
     last_push = db.Column(db.DateTime)
     email = db.Column(db.String(64))
 
     state_id = db.Column(db.Integer, db.ForeignKey('state.id'))
     state = db.relationship('state', foreign_keys=[state_id])
+    district_id = db.Column(db.Integer, db.ForeignKey('district.id'))
+    district = db.relationship('district', foreign_keys=[district_id])
 
-    def __init__(self, username, pass_hash, zip, email):
+    def __init__(self, username, pass_hash, zip_code, email):
         self.username = username
         self.pass_hash = pass_hash
-        self.zip = zip
+        self.zip_code = zip_code
         self.email = email
 
-        # TODO: magical zip code to state logic
+        # TODO: magical zip code to state/district logic
         self.state = None
+        self.district = None
 
     def update_push_time(self):
         self.last_push = datetime.datetime.utcnow()
@@ -48,17 +63,22 @@ class Congressperson(db.Model):
     twitter = db.Column(db.String)
     desc = db.Column(db.String)
     photo_url = db.Colume(db.String)
-    district_no = db.Column(db.Integer)
 
+    district_id = db.Column(db.Integer, db.ForeignKey('district.id'))
+    district = db.relationship('district', foreign_keys=[district_id])
     state_id = db.Column(db.Integer, db.ForeignKey('state.id'))
     state = db.relationship('state', foreign_keys=[state_id])
 
-    def __init__(self, name, twitter, state=None, district_no=None, type=None):
-        self.name = name
+    def __init__(self, twitter, state=None, district=None):
         self.twitter = twitter
+
+        # TODO: pull these details with tweepy to populate the data. may even do parsing on desc to get state/district
+        self.name = None
+        self.desc = None
+        self.photo_url = None
+
         self.state = state
-        self.district_no = district_no
-        self.type = type
+        self.district = district
 
 
 db.create_all()
